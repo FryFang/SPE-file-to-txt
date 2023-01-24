49de8dd7f5d6ac173f4765f9a5cc7262a6419753
Fry Fang <fangry37@gmail.com>
2023-01-24 16:55:19 -0500

    Update spe2txt.py

--- spe2txt.py
+++ spe2txt.py
@@ -6,22 +6,23 @@ def read_spe(filename):
     for i in range (len(f)):
         if f[i] == '$MEAS_TIM:':
             time_info = f[i+1].split(' ')
-            meas_time = int(time_info[0])
-            real_time = int(time_info[1])
-            dead_time = (1- meas_time/real_time)*100
+            meas_time = int(time_info[0]) # get measurement time
+            real_time = int(time_info[1]) # get real time
+            dead_time = (1- meas_time/real_time)*100 # calculate dead time in percentage
             # print(meas_time, real_time, dead_time)
-        if f[i] == '$DATA:':
-            data_start_line = i+2
+        if f[i] == '$DATA:': # start parsing through the spectrum
+            data_start_line = i+2 # start index of actual data
         if f[i] == '$ROI:':
-            data_end_line = i
+            data_end_line = i # end indexo f actual data
     counts = []
     for i in range(data_start_line, data_end_line):
         counts.append(int(f[i]))
     # print(counts[0], counts[-1], len(counts))
+    # return measurement time, real time, dead time in percentage, and an array of counts for each channel
     return meas_time, real_time, dead_time, np.array(counts)
 
 def spe2txt(meas_time, real_time, dead_time, counts_array,filename):
-    channel_array = np.arange(8192)
+    channel_array = np.arange(8192) # your ADC or MCA channel number
     np.savetxt('../txt/'+filename[:-3]+'txt', np.transpose([channel_array, counts_array]), header = 'measure time [s], real time [s], dead time [s]\n {} {} {:.2f}\n channel counts'.format(meas_time, real_time, dead_time), fmt = '%9i')
     return None
 
